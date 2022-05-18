3b30ff3740fa08628cba1ec72b0bd471709bd2db
stephanie-ash <stephanie.ashdown@hotmail.co.uk>
2022-05-18 11:21:37 +0100

    Set-up routes, controllers and logs

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 const express = require('express');
 
+const middleware = require('./middlewares');
+
+const routes = require('./routes')
+
 const app = express();
+
+// middleware
+middleware(app);
+
+// routes
+routes(app);
 
 module.exports = app;
--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const http = require('http');
 const app = require('./app');
 
-const server = http.createServer();
+const server = http.createServer(app);
 
-server.listen('5000', () => {
+server.listen(5000, () => {
     console.log('Server is now listening.');
 });
