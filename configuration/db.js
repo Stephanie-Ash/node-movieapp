ffe6fdde851f6dc3d688bfe4614e3e0ed0e60d50
stephanie-ash <stephanie.ashdown@hotmail.co.uk>
2022-05-26 10:47:38 +0100

    Add get, post, edit and delete commments routes

--- configuration/db.js
+++ configuration/db.js
@@ -2,11 +2,15 @@ const { MongoClient } = require('mongodb');
 
 const _uri = process.env.MONGODB_URI;
 
-const dbCon = (coll, cb) => {
+const dbCon = (coll, cb, coll2) => {
     MongoClient.connect(_uri)
         .then(async (client) => {
             const db = client.db('sample_mflix').collection(coll);
-            await cb(db);
+            let db2;
+            if(coll2) {
+                db2 = client.db('sample_mflix').collection(coll2);
+            }
+            await cb(db, db2);
             client.close();
         })
 };
--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,15 @@
 const { postLogin } = require('./auth/login')
 const { postSignup } = require('./auth/signup');
 const { getMovies, getOneMovie } = require('./movieC');
+const { postComment, putComment, deleteComment, getComments } = require('./commentC');
 
 module.exports = {
     postLogin,
     getMovies,
     getOneMovie,
-    postSignup
+    postSignup,
+    postComment,
+    putComment,
+    deleteComment,
+    getComments
 }
